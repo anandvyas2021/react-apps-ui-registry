462d2bce95d2a35c7091b1224a7577d9bb030e9e
anandvyas1290 <anandvyas1290@gmail.com>
2026-09-17 15:56:27 +0530

    fixing libs/utils installation issue

--- registry/mobile/stylesheet/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-import { icons } from "lucide-react-native";
-
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
-
-export type IconName = keyof typeof icons;
